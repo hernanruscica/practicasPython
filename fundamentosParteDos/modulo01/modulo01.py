d1cd1530677effe217347bfa799df3a7850fa947
cesar hernan ruscica <47112525+hernanruscica@users.noreply.github.com>
2022-07-18 13:29:14 -0300

    Add files via upload
    empezando con segunda parte de los fundamentos de python.

---
+++ fundamentosParteDos/modulo01/modulo01.py
@@ -0,0 +1,14 @@
+#Practicas del modulo 01 de la segunda parte de Fundamentos de Python
+"""
+- Modulos
+- Paquetes
+- PIP
+Biblioteca Standar de python: https://docs.python.org/3/library/index.html
+
+Importando modulos: 1.1.1.3 Empleado Módulos
+
+Se usa la palabra clave "import", seguido del nombre del modulo.
+la importacion debe estar hecha antes de cualquier uso de las entidades o funciones del modulo.
+Si se importan varios modulos, se los separa con comas o cada uno en una linea.
+
+"""
